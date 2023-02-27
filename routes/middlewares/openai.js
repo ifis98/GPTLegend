8cd8f6b017a81d93866a15397c152e2d8279b002
ifis98 <yashlad@gmail.com>
2023-02-27 16:38:20 -0600

    added node-sass

--- routes/middlewares/openai.js
+++ routes/middlewares/openai.js
@@ -1,7 +1,7 @@
 const OpenAI = require('openai-api');
 
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
-console.log(process.env)
+//console.log(process.env)
 const openai = new OpenAI(OPENAI_API_KEY);
 
 module.exports = openai
