e2564c00f5ab2debd2b3d5bc84ef684357a34144
ifis98 <yashlad@gmail.com>
2023-03-01 14:12:17 -0600

    added chatgpt to email

--- routes/ai/writing/email.js
+++ routes/ai/writing/email.js
@@ -53,6 +53,7 @@ app.post('/writing/email', async (req, res, next) => {
 			*/
 		});
 
+		console.log("output: "+gptResponse.data)
 
 		let output = `${gptResponse.data.choices[0].text}`
 		console.log("output: "+output)
