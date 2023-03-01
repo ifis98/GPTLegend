0e4784ecc783d4ace2227d0911fee5c4c1e28efb
ifis98 <yashlad@gmail.com>
2023-03-01 14:12:38 -0600

    added chatgpt to email

--- routes/ai/writing/email.js
+++ routes/ai/writing/email.js
@@ -56,7 +56,7 @@ app.post('/writing/email', async (req, res, next) => {
 		console.log("output: "+gptResponse.data)
 
 		let output = `${gptResponse.data.choices[0].text}`
-		console.log("output: "+output)
+		//console.log("output: "+output)
 		// remove the first character from output
 		output = output.substring(1, output.length)
 
